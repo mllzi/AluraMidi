8480459fa565340ae50bcd79adb91a5117885ea2
bia <bianca.calixto.soares@escola.pr.gov.br>
2023-11-08 12:07:25 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,25 +1,31 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
+while (contador < listaDeTeclas.length) {
+const efeito = listaDeTeclas[contador].classList[1];
+ const idAudio = "#som_"+efeito;
+ listaDeTeclas[contador].onclik = function()
+ {
+     tocaSom(idAudio);
+ }
+    contador = contador + 1;
+    consol.long(contador);
+            
 
 
 let contador = 0;
-
-while (contador <9) {
-
-    ListaDeTeclas[contador].onclick = tocaSomPom;
+while (contador<9) {
+    listaDeTeclas[ contador].onclick = tocaSomPom;
     contador = contador + 1;
-    console.log(contador);
+    console.long(contador);
 }
-
-
-
 
 //document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
